90229d1e4f5a4d71b0e9360f74d9966947e6c237
Jess Carr <jc086615@ohio.edu>
2020-01-29 10:40:22 -0500

    Getting started with changes

---
+++ Models/Orders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarrMIS4200.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int orderNums { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+
+        public int customerID { get; set; }
+
+        public virtual customer customer { get; set; }
+}
+    
+}
--- Models/customer.cs
+++ Models/customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -18,4 +19,6 @@ namespace CarrMIS4200.Models
 
         public DateTime customerSince { get; set; }
     }
+   
+
 }
